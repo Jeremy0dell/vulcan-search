e67b4562839bd4ffd19711df9d8b086a6c1b9208
Jeremy Odell <jeremy0dell@yahoo.com>
2018-02-17 13:53:36 -0600

    some styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
+import Nav from './components/Nav/'
+import Footer from './components/Footer'
 import GoogleSearch from './components/GoogleSearch'
 import TitleScraper from './components/TitleScraper/'
 import ConfigurableTable from './components/ConfigurableTable/'
-import HeatMap from './components/HeatMap/'
+// import HeatMap from './components/HeatMap/'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Nav />
         <GoogleSearch />
         <TitleScraper />
         <ConfigurableTable />
-        <HeatMap />
+        <Footer />
       </div>
     );
   }
