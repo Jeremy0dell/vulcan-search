7ef9454490241fb3f839871511ba2daa624077c8
Jeremy Odell <jeremy0dell@yahoo.com>
2018-02-19 19:18:15 -0600

    fixed typo

--- src/components/Nav/TopBar.jsx
+++ src/components/Nav/TopBar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const TopBar = () =>
   <div className="flex-between dark-bar">
     <Link to="/"><div>Home</div></Link>
-    <Link to="/search"><div>Google Seach</div></Link>
+    <Link to="/search"><div>Google Search</div></Link>
     <Link to="/title"><div>Title Scraper</div></Link>
     <Link to="/table"><div>Configurable Table</div></Link>
     <Link to="/heatmap"><div>Heat Map</div></Link>
